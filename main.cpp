7bf3146b9ee59da25118b151a1839a887f09727e
Nathaniel Hillert <nathille333+github@gmail.com>
2022-10-07 20:16:03 -0500

    A fresh start

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "main.h"
+
+
+//the over arching state of the game, this should be able to be accessed by, but not set by, any class
+static State GameState = MAIN_MENU;
+
+int main() {
+	
+	return SUCCESSFUL_EXIT;
+}
