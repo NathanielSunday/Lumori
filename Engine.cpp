cd55ca327cfda4d732fa5d853c4b195a100d66e9
Nathaniel Hillert <nathille333+github@gmail.com>
2022-11-12 19:26:31 -0600

    Update Engine.cpp

--- Engine.cpp
+++ Engine.cpp
@@ -3,6 +3,7 @@
 
 void Engine::run() {
 	initEngine();
+	//load some sort of setting perhaps?
 	initWindow();
 	initViewport();
 	mainLoop();
@@ -20,17 +21,17 @@ float Engine::deltaTime() {
 void Engine::initEngine() {
 	_state = State::INTRO;
 	Level::init();
-
 }
 
 void Engine::initWindow() {
-	_window.create(sf::VideoMode(1000, 1000), "Lumori");
+	_window.create(sf::VideoMode(800, 700), "Lumori", sf::Style::Titlebar | sf::Style::Close);
 	_window.setFramerateLimit(60);
 }
 
 void Engine::initViewport() {
+
+	_viewport.reset(sf::FloatRect(sf::Vector2f(0, 0), sf::Vector2f(256, 224)));
 	_window.setView(_viewport);
-	_viewport.setViewport(sf::FloatRect(sf::Vector2f(500, 500), sf::Vector2f(800, 600)));
 }
 
 void Engine::mainLoop() {
